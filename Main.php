ae7e8f05d6fd7c48ee575f1908e06235f92b02a3
hoanganh <anhnh3@bsscommerce.com>
2023-07-04 08:46:51 +0700

    add header control

--- Main.php
+++ Main.php
@@ -1,4 +1,21 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+
+// Specify which request methods are allowed
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
+
+// Additional headers which may be sent along with the CORS request
+header('Access-Control-Allow-Headers: X-Requested-With,Authorization,Content-Type');
+
+// Set the age to 1 day to improve speed/caching.
+header('Access-Control-Max-Age: 86400');
+
+// Exit early so the page isn't fully loaded for options requests
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'options') {
+    exit();
+}
+header("Content-Type: application/json; charset=UTF-8");
+
 class Main
 {
     // Checking the Request Method
